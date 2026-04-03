4c779f6fc07fd16bcd3c98bef542685fbfe97d89
Matias <matiasgcatalan@estudiante.uc.cl>
2026-04-03 19:27:04 -0300

    modo 1v2 dartvader agustin

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,7 @@
 print("MODALIDAD JUEGO 1VS1")
 jugador1 = "Mati"
-jugador2 = "Enemigo"
-print(f"PELEA {jugador1} contra {jugador2}")
+jugador2 = "Enemigo1: Darth Vader"
+jugador3 = "Enemigo2: Agustin"
+print(f"PELEA {jugador1} contra {jugador2}junto a {jugador3}")
+
 
